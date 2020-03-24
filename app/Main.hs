6be068d28fcd11e58a609f391090f54961238572
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-03-24 21:22:00 +0000

    [#69] Move CLI options inside library (#82)
    Resolves #69

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,6 @@
 module Main (main) where
 
-import Path (parseRelDir, parseRelFile)
-
-import Life.Main.Add (lifeAdd)
-import Life.Main.Init (lifeInit)
-import Life.Main.Pull (lifePull)
-import Life.Main.Push (lifePush)
-import Life.Main.Remove (lifeRemove)
-
-import Options (LifeCommand (..), PathOptions (..), PullOptions (..), parseCommand)
-
-import qualified Data.Set as Set
+import Life.Main (lifeMain)
 
 main :: IO ()
-main = parseCommand >>= \case
-    Init   owner           -> lifeInit   owner
-    Add    PathOptions{..} -> lifeAdd    pathOptionsPath
-    Remove PathOptions{..} -> lifeRemove pathOptionsPath
-    Push                   -> lifePush
-    Pull   PullOptions{..} -> do
-        withoutFiles <- Set.fromList <$> mapM parseRelFile pullOptionsNoFiles
-        withoutDirs  <- Set.fromList <$> mapM parseRelDir  pullOptionsNoDirs
-        lifePull pullOptionsOwner withoutFiles withoutDirs
+main = lifeMain
